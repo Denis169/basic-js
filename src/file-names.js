89d145d94373302bb2240e1fd2e5f0eb06b81030
Denis <denis6125038@gmail.com>
2022-04-28 16:55:35 +0300

    feat: file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const objFiles = {};
+  let count = 0;
+  const finishArr = [];
+
+  names.forEach((item, index) => {
+    if (objFiles[`${item}`]) {
+      objFiles[`${item}`].amount +=1;
+      objFiles[`${item}`].index.push(index);
+      count++;
+    } else {
+      objFiles[`${item}`] = {amount: 1, index: [index]};
+      count++;
+    }
+  })
+
+  for (let i = 0; i < count; i++) {
+    for (let key in objFiles) {
+      if (objFiles[`${key}`].index.includes(i)) {
+        let itemPush = `${key}(${objFiles[`${key}`].amount - objFiles[`${key}`].index.length})`;
+        if (objFiles[`${key}`].amount - objFiles[`${key}`].index.length === 0) {
+          if (finishArr.includes(`${key}`)) {
+            finishArr.push(`${key}(${objFiles[`${key}`].amount - objFiles[`${key}`].index.length + 1})`)
+          } else {
+            finishArr.push(`${key}`)
+          }
+        } else {
+          finishArr.push(itemPush);
+        }
+        let deletedIndex = objFiles[`${key}`].index.indexOf(i);
+        objFiles[`${key}`].index = [...objFiles[`${key}`].index.slice(0, deletedIndex),...objFiles[`${key}`].index.slice(deletedIndex + 1)]
+      }
+    }
+  }
+
+  return finishArr;
+
 }
 
 module.exports = {
