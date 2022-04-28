3fc1604f14c914f2c705859014925db151aec39e
Denis <denis6125038@gmail.com>
2022-04-28 11:19:53 +0300

    feat: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = [];
+  const incommingMassive = str.split('');
+  let lastItem = '';
+
+  incommingMassive.forEach((item) => {
+    if ( lastItem !== item) {
+      arr.push(item);
+    } else {
+      if (arr[arr.length - 1] === item) {
+        arr.pop();
+        arr.push(`2${item}`);
+      } else {
+        let i = Number(arr[arr.length - 1].slice(0, -1));
+        arr.pop();
+        arr.push(`${i + 1}${item}`);
+      }
+    }
+    lastItem = item;
+  })
+
+  return arr.join('');
 }
 
 module.exports = {
