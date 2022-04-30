79ad0fbb93e04d3268b90b026d92bc2ae6180a09
Denis <denis6125038@gmail.com>
2022-04-30 10:45:46 +0300

    feat: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+
+  const newArr = matrix.reduce((acc, item, index) => {
+    acc.push(item.reduce((acc2, itemElem) => {
+      acc2.push(0);
+      return acc2
+    },[]))
+    return acc;
+  },[])
+
+  matrix.forEach((item, index) => {
+    item.forEach((elem, indexElem) => {
+      if ( index - 1 >= 0 && indexElem - 1 >= 0 && matrix[index - 1][indexElem - 1]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( index - 1 >= 0 && matrix[index - 1][indexElem]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( index - 1 >= 0 && indexElem + 1 <= item.length - 1 && matrix[index - 1][indexElem + 1]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( indexElem - 1 >= 0 && matrix[index][indexElem - 1]) {
+        newArr[index][indexElem] += 1;
+      }
+      if (indexElem + 1 <= item.length - 1 && matrix[index][indexElem + 1]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( index + 1 <= matrix.length - 1 && indexElem - 1 >= 0 && matrix[index + 1][indexElem - 1]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( index + 1 <= matrix.length - 1 && matrix[index + 1][indexElem]) {
+        newArr[index][indexElem] += 1;
+      }
+      if ( index + 1 <= matrix.length - 1 && indexElem + 1 <= item.length - 1 && matrix[index + 1][indexElem + 1]) {
+        newArr[index][indexElem] += 1;
+      }
+    })
+  })
+
+  return newArr;
 }
 
 module.exports = {
